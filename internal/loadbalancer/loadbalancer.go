40e5f4066837a4cff05d39daf76e28da383faa04
IAmMasterCraft <bolu.akinsefunmi@gmail.com>
2024-04-24 08:39:33 +0100

    health checks implemented

--- internal/loadbalancer/loadbalancer.go
+++ internal/loadbalancer/loadbalancer.go
@@ -5,35 +5,91 @@ import (
 	"net/url"
 	"log"
 	"net/http/httputil"
+	"sync"
+	"time"
 )
 
-var backendServers = []string{
-	"http://localhost:8081",
-	"http://localhost:8082",
-	"http://localhost:8083",
-	"http://localhost:8084",
-	"http://localhost:8085",
+type Server struct {
+	URL string
+	Alive bool
+	mu sync.Mutex
+}
+
+var backendServers = [] *Server{
+	{
+		URL: "http://localhost:8081",
+		Alive: true,
+	},
+	{
+		URL: "http://localhost:8082",
+		Alive: true,
+	},
+	{
+		URL: "http://localhost:8083",
+		Alive: true,
+	},
+	{
+		URL: "http://localhost:8084",
+		Alive: true,
+	},
+	{
+		URL: "http://localhost:8085",
+		Alive: true,
+	},
 }
 
 var currentServer int;
 
 // use round robin to select the next server
-func getNextServer() string {
+func getNextServer() *Server {
 	nextServer := backendServers[currentServer % len(backendServers)]
+	if !nextServer.Alive {
+		return getNextServer()
+	}
 	currentServer++
 	return nextServer
+}
+
+// HealthCheck checks the health of the backend servers
+func healthCheck() {
+	for _, server := range backendServers {
+		res, err := http.Get(server.URL)
+		server.mu.Lock()
+		if err != nil || res.StatusCode != 200 {
+			server.Alive = false
+			log.Printf("Server %s is down", server.URL)
+		} else {
+			server.Alive = true
+		}
+		server.mu.Unlock()
+	}
+}
+
+// init starts the health check for the backend servers
+func init() {
+	go func() {
+		for {
+			healthCheck()
+			time.Sleep(10 * time.Second)
+		}
+	}()
 }
 
 // RequestHandler to forward the request to the backend server
 func RequestHandler(w http.ResponseWriter, r *http.Request) {
 	nextServer := getNextServer()
-	remoteUrl, err := url.Parse(nextServer)
+	if nextServer == nil {
+		log.Printf("No server available")
+        http.Error(w, "Service unavailable", http.StatusServiceUnavailable)
+        return
+    }
+	remoteUrl, err := url.Parse(nextServer.URL)
 	if err != nil {
 		log.Printf("Error parsing backend URL: %s", err)
         http.Error(w, "Service unavailable", http.StatusServiceUnavailable)
         return
 	}
 	proxy := httputil.NewSingleHostReverseProxy(remoteUrl)
-	log.Printf("Forwarding request to %s", nextServer)
+	log.Printf("Forwarding request to %s", nextServer.URL)
 	proxy.ServeHTTP(w, r)
 }
